481ca7fa1a7c13f9277a073aaaee8a2794019653
AmarOmerovic <amaromerovic473@gmail.com>
2022-12-01 13:55:22 +0100

    add popular section

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ setTimeout(function() {
 }, 2900);
 
 
-history.scrollRestoration = 'manual';
+/*history.scrollRestoration = 'manual';
 
 if (history.scrollRestoration) {
     history.scrollRestoration = 'manual';
@@ -14,5 +14,43 @@ if (history.scrollRestoration) {
     window.onbeforeunload = function () {
         window.scrollTo(0, 0);
     }
-}
+}*/
 
+var swiper = new Swiper(".popular-container", {
+    slidesPerView: 1, 
+    spaceBetween: 10,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        280: {
+            slidesPerView: 1, 
+            spaceBetween: 10,
+        },
+        320: {
+            slidesPerView: 2, 
+            spaceBetween: 10,
+        },
+        510: {
+            slidesPerView: 2, 
+            spaceBetween: 10,
+        },
+        758: {
+            slidesPerView: 3, 
+            spaceBetween: 15,
+        },
+        900: {
+            slidesPerView: 4, 
+            spaceBetween: 20,
+        },
+    }
+  });
